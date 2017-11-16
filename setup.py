79bfcd7f8c57e336ed1687236b8e2a82c96bc98c
Kevin (Pololu) <dev-kevin@pololu.com>
2017-11-16 12:44:14 -0800

    fix setup.py description and add license

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='dual_mc33926_rpi',
       version='2.0.0',
-      description=('Library for the Pololu Dual MC33926 Motor'
+      description=('Library for the Pololu Dual MC33926 Motor '
                    'Driver for Raspberry Pi'),
       url='http://www.pololu.com/product/2755',
       py_modules=['dual_mc33926_rpi'],
